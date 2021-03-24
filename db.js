abb69757210cd09bc82611e84e41dafe91e79d0e
Christian Khan <christiankhan@Hotmail.com>
2021-03-24 18:40:03 -0400

    db.js edit

--- db.js
+++ db.js
@@ -1,22 +1,28 @@
-const Pool = require('pg').Pool;
+const Pool = require('pg').Pool; 
 require("dotenv").config();
 
 const devConfig = {
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
+    user: process.env.PGUSER, 
+    password: process.env.PGPASSWORD, 
+    host: process.env.PGHOST, 
+    port: process.env.PGPORT, 
     database: process.env.PGDATABASE
 };
 
 const proConfig = process.env.DATABASE_URL;
 
+// const proConfig = {
+//     connectionString: process.env.DATABASE_URL, 
+//     ssl: {
+//         rejectUnauthorized: false
+//       }
+// }
+// const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig: devConfig ); 
+
 const pool = new Pool({
-    connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    connectionString:
+      process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  });
+  
 
-});
-
-module.exports = pool;
+module.exports = pool; 
